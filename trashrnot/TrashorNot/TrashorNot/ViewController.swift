76f94c6da702feb3c8cb5113f667b18695fde139
rguchi <38776857+rguchi@users.noreply.github.com>
2018-11-25 01:06:09 -0800

    connect api with camera??

--- trashrnot/TrashorNot/TrashorNot/ViewController.swift
+++ trashrnot/TrashorNot/TrashorNot/ViewController.swift
@@ -1,46 +1,90 @@
-//
+
+
 //  ViewController.swift
-//  TrashorNot
+//  CloudSightExample
 //
-//  Created by Ruchi Gupta on 7/25/2018.
-//  Copyright © 2018 Ruchi Gupta. All rights reserved.
-//
-
 import UIKit
+import CloudSight
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, CloudSightQueryDelegate {
+    
 
+    @IBOutlet var resultLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
+    
+    var cloudsightQuery: CloudSightQuery!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        CloudSightConnection.sharedInstance().consumerKey = "EUVIyy2Rp_OGH7pm8z9oxA";
+        CloudSightConnection.sharedInstance().consumerSecret = "xqgUfGKc4SfKUW9CSeZeXw";
     }
     
-    override func didReceiveMemoryWarning()
-    {
+    override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        //dispose of any resources that can be recreated 
-    }
-
-    @IBAction func Camerabutton(_ sender: AnyObject) {
-        if UIImagePickerController.isSourceTypeAvailable(.camera)
-        {
-        let imagePicker = UIImagePickerController()
-            imagePicker.delegate = self
-            imagePicker.allowsEditing = true
-            imagePicker.sourceType = UIImagePickerController.SourceType.camera
-        self.present(imagePicker, animated: true, completion: nil)
-        }
-
+        // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func Librarybutton(_ sender: AnyObject) {
-        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+    @IBAction func cameraButtonPressed(_ sender: Any) {
+        
+        // Check to see if the Camera is available
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera) {
+            let imagePicker = UIImagePickerController()
             
-            let library = UIImagePickerController()
-            library.delegate = self
-            library.sourceType = UIImagePickerController.SourceType.photoLibrary
-            self.present(library, animated: true, completion: nil)
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerController.SourceType.camera
+            imagePicker.allowsEditing = false
+            
+            // Show the UIImagePickerController view
+            self.present(imagePicker, animated: true, completion: nil)
+        } else {
+            print("Cannot access the camera");
         }
-    } 
+    }
+    
+    private func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        // Dismiss the UIImagePickerController
+        self.dismiss(animated: true, completion: nil)
+        
+        // Assign the image reference to the image view to display
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        imageView.image = image
+        
+        // Create JPG image data from UIImage
+       let imageData = image.jpegData(compressionQuality: 0.8)
+        
+        cloudsightQuery = CloudSightQuery(image: imageData,
+                                          atLocation: CGPoint.zero,
+                                          withDelegate: self,
+                                          atPlacemark: nil,
+                                          withDeviceId: "device-id")
+        cloudsightQuery.start()
+        activityIndicatorView.startAnimating()
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func cloudSightQueryDidFinishUploading(_ query: CloudSightQuery!) {
+        print("cloudSightQueryDidFinishUploading")
+    }
+    
+    func cloudSightQueryDidFinishIdentifying(_ query: CloudSightQuery!) {
+        print("cloudSightQueryDidFinishIdentifying")
+        
+        // CloudSight runs in a background thread, and since we're only
+        // allowed to update UI in the main thread, let's make sure it does.
+        DispatchQueue.main.async {
+            self.resultLabel.text = query.name()
+            self.activityIndicatorView.stopAnimating()
+        }
+    }
+    
+    func cloudSightQueryDidFail(_ query: CloudSightQuery!, withError error: Error!) {
+        print("CloudSight Failure: \(String(describing: error))")
+    }
 }
 
